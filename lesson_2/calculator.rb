928ec47de75a0bbcaef7e0e04a6832bed3bf3963
Edward Morozov <edymorozov@gmail.com>
2022-05-16 06:43:21 -0400

    committing calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,36 +1,92 @@
-
-
-Kernel.puts("Which operation would you like to perform")
-operation = Kernel.gets().chomp()
-
-
-Kernel.puts("Please enter your first number. In the case of division this would be your numerator.")
-number_1 = Kernel.gets().chomp().to_i
-Kernel.puts("Please enter your second number. In the case of division, this would be the denominator.")
-number_2 = Kernel.gets().chomp().to_i
-
-def test(operation, num1, num2)
-  if (operation == '*' || operation == '/' || operation == '-' || operation == '+') && (num1.is_a?(Integer) && num2.is_a?(Integer) && num2 != 0)
-    true
-  else 
-    false
-  end
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
+def valid_number?(num)
+  num.to_i() != 0
+end
 
-answer = nil
-if test(operation, number_1, number_2) 
-  if operation == '*' 
-    answer = number_1 * number_2
-  elsif operation == '/'
-    answer = number_1.to_f() / number_2.to_f()
-  elsif operation == '-'
-    answer = number_1 - number_2
+prompt("Welcome to Calculator! Enter your name:")
+
+name = ''
+loop do 
+  name = gets.chomp
+
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
   else
-    answer = number_1 + number_2
+    break
   end
-  Kernel.puts(answer)
-else
-  puts "please enter integers, valid operators (+, -, /, or *) and never divide by 0"
 end
 
+prompt("Hi #{name}")
+
+loop do #main loop
+  number1 = ''
+  loop do
+    prompt("What's the first number?")
+    number1 = gets.chomp
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number.")
+    end
+  end
+
+  number2= ''
+  loop do
+    prompt("What's the second number?")
+    number2 = gets.chomp
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number.")
+    end
+  end
+
+  operator = ''
+  operator_prompt = <<-MSG
+  What operation would you like to perform?
+  1) add
+  2) subtract
+  3) multiply
+  4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1,2,3, or 4")
+    end
+  end
+
+  key = {'1' => 'adding', '2' => 'subtracting', '3'=> 'multiplying', '4' => 'dividing'}
+  prompt("#{key[operator]} the two numbers...")
+
+    result = case operator
+             when '1'
+              number1.to_f + number2.to_f
+             when '2'
+              number1.to_f - number2.to_f
+             when '3'
+              number1.to_f * number2.to_f
+             when '4'
+              number1.to_f() / number2.to_f()
+             end
+
+
+prompt("The result is #{result}")
+
+prompt("Do you want to perform another calculation? (Y to calculate again)")
+answer = gets.chomp
+break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Good bye!")
